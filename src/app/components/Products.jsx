47e9270e6905923d5d983d9fa729a3e1264563c0
CodeWithMohaimin <dev.mohaimin@gmail.com>
2025-03-08 23:30:45 +0600

    changed some responsive stuff

--- src/app/components/Products.jsx
+++ src/app/components/Products.jsx
@@ -19,7 +19,7 @@ const Products = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="max-w-[1700px] mx-auto p-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {BoughtedProducts.map((card) => (
           <div
